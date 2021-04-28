cdbfc5c68a80f6df236cd5383ce628719f00ab19
Ferjose94 <44064470+Ferjose94@users.noreply.github.com>
2021-04-28 12:28:28 -0500

    Create PryNat.js

---
+++ PryNat.js
@@ -0,0 +1,17 @@
+
+
+// Activación de jQuery Función principal de jQuery
+$(document).ready(function(){
+	//Activación de Slick-Slider
+	$('.slick-slider').slick({
+		autoplay: true,
+		fade: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 200,
+		arrows: true
+	});
+
+
+});
